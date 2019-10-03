bbbe4c6e89d6bb6746c52645410ac1ebead554aa
irisjitomo <irisjitomo@eyahoo.com>
2019-10-03 16:41:05 -0400

    added array

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ require('dotenv').config()
 
 const server = express()
 
+const friends = [
+    {
+        "name" : "lesly"
+    },
+    {
+        "name" : "lovely"
+    },
+    {
+        "name" : "lowbrow"
+    },
+    {
+        "name" : "lazlo"
+    },
+];
+
 
 //custome middleWares
 function logger(req, res, next){
@@ -16,6 +31,20 @@ server.use(logger)
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: "hello its working already"})
+})
+
+server.get('/friends', (req, res) => {
+    res.status(200).json(friends)
+})
+
+server.post('/friends', (req, res) => {
+    if (req.body.name) {
+        friends.push(req.body)
+        res.status(201).json(req.body)
+    } else {
+        res.status(404).json({message: 'must contain name'})
+    }
+    
 })
 
 
